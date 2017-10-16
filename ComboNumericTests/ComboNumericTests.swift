131cd833dcd14c8009bf251754479f663f15ec48
AJ <aj@node-1w7jr9qsp3unh0ik342hw0j22.ipv6.telus.net>
2017-10-15 21:52:28 -0700

    Initial Commit

---
+++ ComboNumericTests/ComboNumericTests.swift
@@ -0,0 +1,36 @@
+//
+//  ComboNumericTests.swift
+//  ComboNumericTests
+//
+//  Created by AJ on 2017-10-15.
+//  Copyright © 2017 AJ. All rights reserved.
+//
+
+import XCTest
+@testable import ComboNumeric
+
+class ComboNumericTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
